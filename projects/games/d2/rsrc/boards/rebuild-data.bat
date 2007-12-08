a89ea9ebdc4324df61942af4c3465c3c7d84181e
robert <robert@threerings.net>
2007-12-08 01:00:26 +0000

    All of these files are now in the defense repo.

--- projects/games/d2/rsrc/boards/rebuild-data.bat
+++
@@ -1,2 +0,0 @@
-del *.data
-for %%p in (default ep1) do zip -0 -j %%p.data %%p\*.swf %%p\*.xml
--- projects/games/d2/rsrc/boards/rebuild-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for PACK in default ep1; do 
-  echo "Packing $PACK..."
-  rm $PACK.data
-  zip -0 -j $PACK.data $PACK/*.swf $PACK/*.xml
-done
